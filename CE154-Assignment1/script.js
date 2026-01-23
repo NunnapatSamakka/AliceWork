35c66af44bc532a78807ebfc61e8d4f9a3608937
Nunnapat Samakka <nunnapatsamakka@gmail.com>
2026-01-23 12:20:46 +0000

    committed from vscode

--- CE154-Assignment1/script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById("helloBtn");
-const msg = document.getElementById("msg");
-
-btn.addEventListener("click", () => {
-    msg.textContent = "Hello! This works offline :)";
-});
